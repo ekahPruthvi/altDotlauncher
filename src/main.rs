d5190e38599a0216c9df30b5b79dfa95620df94e
ekahPruthvi <p1sribhat@gmail.com>
2025-05-13 09:25:19 +0530

    v1.1.0 add ai integration

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,14 @@ use vte4::Terminal;
 use vte4::PtyFlags;
 use std::path::Path;
 use chrono::{Local, DateTime};
-
+use groq_api_rust::{GroqClient, ChatCompletionRequest, ChatCompletionRoles, ChatCompletionMessage};
 fn main() {
     let app = Application::builder()
-        .application_id("com.example.launcher")
+        .application_id("ekah.scu.moss")
         .build();
 
     app.connect_activate(build_ui);
-    Command::new("bash").arg("-c").arg("cd").arg("~/");
+    // Command::new("bash").arg("-c").arg("cd").arg("~/");
     app.run();
 }
 
@@ -34,6 +34,69 @@ enum Mode {
     None
 }
 
+fn read_api_key() -> String {
+    let home_dir = std::env::var("HOME").unwrap_or_default();
+    let key_path = format!("{}/.config/moss/key.dat", home_dir);
+    fs::read_to_string(key_path)
+        .expect("Failed to read API key file")
+        .trim()
+        .to_string()
+}
+
+fn typing_effect(label: &Label, text: &str, delay_ms: u64) {
+    let label = label.clone();
+    let chars: Vec<char> = text.chars().collect();
+    let index = Rc::new(RefCell::new(0));
+
+    let chars_rc = Rc::new(chars);
+
+    glib::timeout_add_local(std::time::Duration::from_millis(delay_ms), move || {
+        let i = *index.borrow();
+
+        if i < chars_rc.len() {
+            let current_text = chars_rc.iter().take(i + 1).collect::<String>();
+            label.set_text(&current_text);
+            *index.borrow_mut() += 1;
+            glib::ControlFlow::Continue
+        } else {
+            glib::ControlFlow::Break
+        }
+    });
+}
+
+fn ai_typing_effect(label: &Label, text: &str, delay_ms: u64, scr: &ScrolledWindow, boxx: &GtkBox ) {
+    let label = label.clone();
+    let chars: Vec<char> = text.chars().collect();
+    let index = Rc::new(RefCell::new(0));
+
+    let chars_rc = Rc::new(chars);
+    let boxx = boxx.clone();
+    let scr = scr.clone();
+    glib::timeout_add_local(std::time::Duration::from_millis(delay_ms), move || {
+        let i = *index.borrow();
+
+        if i < chars_rc.len() {
+            let current_text = chars_rc.iter().take(i + 1).collect::<String>();
+            label.set_text(&current_text);
+            *index.borrow_mut() += 1;
+            if boxx.height() < 800 {
+                scr.set_size_request(1000, boxx.height());
+            } else {
+                scr.set_size_request(1000, 800); 
+            }
+            glib::ControlFlow::Continue
+        } else {
+            glib::ControlFlow::Break
+        }
+    });
+}
+
+fn strip_markdown_symbols(text: &str) -> String {
+    text.replace("**", "")
+        .replace("__", "")
+        .replace("*", "")
+        .replace("`", "")
+}
 
 fn build_ui(app: &Application) {
     let window = ApplicationWindow::builder()
@@ -52,10 +115,6 @@ fn build_ui(app: &Application) {
     window.add_controller(scroll_controller.clone());
 
     let vbox = GtkBox::new(Orientation::Vertical, 12);
-    // vbox.set_margin_top(20);
-    // vbox.set_margin_bottom(20);
-    // vbox.set_margin_start(20);
-    // vbox.set_margin_end(20);
 
     let entry = Entry::builder()
         .placeholder_text(" Search apps, open marker or use alterAi")
@@ -64,7 +123,8 @@ fn build_ui(app: &Application) {
         .build();
     entry.set_visible(false);
 
-    let info_lable = Label::new(Some(" Moss \ntype `/` to get started"));
+    let info_lable = Label::new(Some(""));
+    info_lable.set_markup(" <i>Moss</i> \ntype `/` to get started");
     info_lable.set_widget_name("info_lable-card");
     info_lable.set_margin_top(20);
     info_lable.set_margin_bottom(10);
@@ -95,6 +155,40 @@ fn build_ui(app: &Application) {
     terminal_box.append(&terminal);
     terminal_box.set_visible(false);
 
+    let alterai_closure = GtkBox::new(Orientation::Vertical, 12);
+    let aiscroller = ScrolledWindow::new();
+    let reply = Label::new(Some(""));
+    aiscroller.set_vexpand(true);
+    aiscroller.set_policy(gtk4::PolicyType::Never, gtk4::PolicyType::Automatic);
+    
+    alterai_closure.set_widget_name("alterai_box");
+    alterai_closure.set_margin_start(10);
+    alterai_closure.set_margin_end(10);
+    alterai_closure.set_margin_top(10);
+    alterai_closure.set_margin_bottom(10);
+    alterai_closure.set_vexpand(false);
+
+    reply.set_widget_name("ai_reply");
+    reply.set_wrap(true);
+    reply.set_max_width_chars(50);
+    reply.set_xalign(0.0);
+    reply.set_margin_start(20);
+    reply.set_margin_end(20);
+    reply.set_margin_top(20);
+    reply.set_margin_bottom(20);
+    reply.set_visible(false);
+
+    let inpute = Entry::builder()
+        .placeholder_text("ask alterAi")
+        .hexpand(true)
+        .build();
+    inpute.set_widget_name("aitry");
+
+    alterai_closure.append(&reply);
+    alterai_closure.append(&inpute);
+    aiscroller.set_child(Some(&alterai_closure));
+    aiscroller.set_visible(false);
+
     let vbox_inner = GtkBox::new(Orientation::Vertical, 12);
     let scroller = ScrolledWindow::new();
     scroller.set_vexpand(true);
@@ -105,6 +199,7 @@ fn build_ui(app: &Application) {
     vbox.append(&info_lable_revealer);
     vbox.append(&scroller);
     vbox.append(&terminal_box);
+    vbox.append(&aiscroller);
     window.set_child(Some(&vbox));
 
     let info_lable_revealer = Rc::new(info_lable_revealer);
@@ -115,6 +210,7 @@ fn build_ui(app: &Application) {
     let current_file_path_name= Rc::new(RefCell::new(Vec::new()));
     let terminal = Rc::new(terminal);
     let terminal_box = Rc::new(terminal_box);
+    let _ai = false;
 
 
     {
@@ -125,11 +221,11 @@ fn build_ui(app: &Application) {
         let current_items = current_items.clone();
         let current_mode = current_mode.clone();
         let current_file_path_name=current_file_path_name.clone();
-        let info = info_lable.clone(); 
+        let info = info_lable.clone();
 
         entry.connect_changed(move |e| {
-            let text = e.text().to_string();
-            let (mode, items, file_path_name) = lister(&text);
+            let text_in_entry = &e.text().to_string();
+            let (mode, items, file_path_name) = lister(&text_in_entry);
             let mut _res_flag = false;
             *current_mode.borrow_mut() = mode.clone();
             *current_items.borrow_mut() = items.clone();
@@ -137,20 +233,27 @@ fn build_ui(app: &Application) {
             *selected_index.borrow_mut() = 0;
             
             // info flags
-            if text == ">t"{
+            if text_in_entry == "~t"{
                 let now: DateTime<Local> = Local::now();
                 let datetime_am_pm = now.format(" %B %e,\n%l:%M %P").to_string();
                 info.set_text(&datetime_am_pm);
-                _res_flag = true
-
+                _res_flag = true;
+            } else if text_in_entry == "!"{
+                info.set_text("");
+                typing_effect(&info, "Ask\nalterAi", 50);
+                _res_flag = true;
+                info.set_widget_name("info_lable-card");
+            } else if text_in_entry.contains("!"){
+                 _res_flag = true;
             } else {
                 info.set_text(" type\n` for marker\n ! for alterAi");
-                _res_flag = false
+                _res_flag = false;
+                info.set_widget_name("info_lable-card");
             }
 
-            info_lable_revealer.set_reveal_child(text.is_empty() || _res_flag);
+            info_lable_revealer.set_reveal_child(text_in_entry.is_empty() || _res_flag);
 
-            if text.is_empty() || _res_flag {
+            if text_in_entry.is_empty() || _res_flag {
                 vbox_opt.set_visible(false);
             }else {
                 vbox_opt.set_visible(true);
@@ -179,9 +282,9 @@ fn build_ui(app: &Application) {
         let vbox_opt = vbox_opt.clone();
         let selected_index = selected_index.clone();
         let current_items = current_items.clone();
-        //let current_mode = current_mode.clone();
+        let current_mode = current_mode.clone();
         let current_file_path_name=current_file_path_name.clone();
-
+        
         event_controller.connect_key_pressed(move |_, key, _, _| {
             let mut index = selected_index.borrow_mut();
             let items = current_items.borrow();
@@ -263,12 +366,52 @@ fn build_ui(app: &Application) {
                                 } else {
                                     eprintln!("No Exec line found");
                                 }
+                            } else if matches!(*current_mode.borrow(), Mode::Ai) {
+                                entry.set_visible(false);
+                                scroller.set_visible(false);
+                                info_lable.set_visible(false);
+                                aiscroller.set_visible(true);
+                                inpute.grab_focus();
+                                let qs = Rc::new(RefCell::new(String::new()));
+                                let qs_clone = qs.clone();
+                                let inp = inpute.clone();
+                                let rep = reply.clone();
+                                let sr = scroller.clone();
+                                let ent = entry.clone();
+                                let ai = aiscroller.clone();
+                                let info = info_lable.clone();
+                                let clo= alterai_closure.clone();
+                                inpute.connect_activate(move |e| {
+                                    let input = e.text().to_string();
+                                    *qs_clone.borrow_mut() = input.clone();
+                                    let qs_later = qs.clone();
+                                    let api_key = read_api_key().to_string();
+                                    let qss = qs_later.borrow();
+                                    if qss.starts_with("exit"){
+                                        ent.set_visible(true);
+                                        sr.set_visible(true);
+                                        info.set_visible(true);
+                                        ai.set_visible(false);
+                                    }
+                                    let client = GroqClient::new(api_key.to_string(), None);
+                                    let messages = vec![ChatCompletionMessage {
+                                        role: ChatCompletionRoles::User,
+                                        content: qss.to_string(),
+                                        name: None,
+                                    }];
+                                    let request = ChatCompletionRequest::new("llama3-70b-8192", messages);
+                                    let response = client.chat_completion(request).unwrap();
+                                    rep.set_visible(true);
+                                    ai_typing_effect(&rep, &strip_markdown_symbols(&response.choices[0].message.content), 5, &ai,&clo);
+                                    assert!(!response.choices.is_empty());
+                                    inp.set_text("");
+                                });
+                                
                             } else {
                                 eprintln!("Failed to read .desktop file: {}", path_str);
                             }
-                            
                         } else {
-                                eprintln!("No pathname returned for selected index");
+                            eprintln!("Failed to read application path")
                         }
                     
                 }
@@ -352,6 +495,32 @@ fn build_ui(app: &Application) {
             border-width: 1px ;
             border-color: rgba(139, 139, 139, 0.5);
         }
+        #alterai_box {
+            background-color: rgba(139, 139, 139, 0.14);
+            border-radius: 12px;
+            border: 0.5px solid rgba(139, 139, 139, 0.59);
+        }
+        #ai_reply {
+            background-color: rgba(139, 139, 139, 0.14);
+            padding: 10px;
+            border-radius: 18px;
+            font-size: 16px;
+            border: 0.5px solid rgba(139, 139, 139, 0.59);
+            font-family: "Cantarell";
+            font-weight: 500;
+        }
+        #aitry{
+            border-bottom: 1px solid rgba(73, 73, 73, 0.59);
+            padding: 10px;
+            padding-left: 15px;
+            border-bottom-right-radius: 12px;
+            border-bottom-left-radius: 12px;
+            background-color: rgba(0, 0, 0, 0.1);
+            color: rgba(224, 222, 222, 0.9);
+            box-shadow:none;
+            font-family: "Cantarell";
+            font-weight: 400; 
+        }
     "#;
 
     let provider = CssProvider::new();
@@ -372,17 +541,14 @@ fn lister(input: &str) -> (Mode, Vec<String>, Vec<String>) {
     }
 
     if input.starts_with('!') {
-        let query = input.trim_start_matches('!');
-        if query.is_empty() {
-            return (Mode::Ai, vec![], Vec::new());
-        }
         return (
             Mode::Ai,
             vec![
-                format!("Search: {}", query),
-                format!("{} : Wikipedia", query),
+                format!("Ask alterAi."),
             ],
-            Vec::new()
+            vec![
+                input.to_string()
+            ]
         );
     }
 
@@ -423,7 +589,7 @@ fn lister(input: &str) -> (Mode, Vec<String>, Vec<String>) {
         }
     }
 
-    if input.starts_with('>') {
+    if input.starts_with('~') {
         // check for info flages
         if input.len() > 1  {
             return (Mode::None,Vec::new(),Vec::new());
