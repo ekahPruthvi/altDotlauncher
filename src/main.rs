3fbc2d2c81e769e43063c2b96d009dc6371a2982
ekahPruthvi <p1sribhat@gmail.com>
2025-09-03 12:13:19 +0530

    marker -> flags

--- src/main.rs
+++ src/main.rs
@@ -1023,7 +1023,7 @@ fn build_ui(app: &Application) {
                     ql_revealer.set_reveal_child(false);
                     entry.grab_focus();
                     first_press.set(false);
-                    info_lable.set_text(" type\n` for marker\n ! for alterAi");
+                    info_lable.set_text(" type\n` for flags\n ! for alterAi");
                 }
                 gtk4::gdk::Key::Escape => {
                     exit(0);
